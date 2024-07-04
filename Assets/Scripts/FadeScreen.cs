46558eb92f7065b758babddd1bd9647eadd5f64a
myoungjelee <120433909+myoungjelee@users.noreply.github.com>
2024-07-05 01:13:42 +0900

    와 빌드하니까 되네

--- Assets/Scripts/FadeScreen.cs
+++ Assets/Scripts/FadeScreen.cs
@@ -1,33 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeScreen : MonoBehaviour
 {
     public bool fadeOnStart = true;
     public float fadeDuration = 2;
     public Color fadeColor;
-    private Renderer rend;
-    private Vector3 originalPosition; // 원래 위치를 저장할 변수
+    private Image image;
+    //private Renderer rend;
+    //public Animator animator;
 
     private void Start()
     {
-        rend = GetComponent<Renderer>();
-        originalPosition = transform.position; // 원래 위치 저장
+        //rend = GetComponent<Renderer>();
+        image = GetComponent<Image>();
+        FadeIn();
+        //FadeOut();
     }
 
     public void FadeIn()
     {
         Fade(1, 0);
-        Debug.Log("페이드인");
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.006f);
+        //Debug.Log("페이드인");
+
     }
 
     public void FadeOut()
     {
         Fade(0, 1);
-        Debug.Log("페이드아웃");
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.006f);
+        //Debug.Log("페이드아웃");
     }
 
     public void Fade(float alphaIn, float alphaOut)
@@ -43,13 +46,10 @@ public class FadeScreen : MonoBehaviour
             Color newColor = fadeColor;
             newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
 
-            rend.material.SetColor("_Color", newColor);
+            image.color = newColor;
 
             timer += Time.deltaTime;
             yield return null;     // 한 프레임만 기다리기
         }
-
-        // 페이드가 끝난 후 원래 위치로 되돌리기
-        transform.position = originalPosition;
     }
 }
