5973ea82e8a578a6688945d2c404a28dbe827d59
myoungjelee <120433909+myoungjelee@users.noreply.github.com>
2024-07-04 14:39:40 +0900

    UI 이벤트 다시 할당

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,7 +43,7 @@ public class GameManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
